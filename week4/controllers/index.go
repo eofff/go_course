426c777de9201db994dba16b86e830ad88037015
fearning <b1kapb999@gmail.com>
2016-11-16 01:51:13 +0300

    move business logic to model(i think so)

--- week4/controllers/index.go
+++ week4/controllers/index.go
@@ -11,13 +11,9 @@ type IndexController struct {
 }
 
 func (controller *IndexController) IndexHandler(w http.ResponseWriter, r *http.Request) {
-	var users [3]models.User
-	users[0] = models.User{"Some name", "test@test.com", "fdscxv"}
-	users[1] = models.User{"Another name", "hz@hz.hz", "aasd"}
-	users[2] = models.User{"Afdsi", "fff@asczx.net", "7asfgvsd"}
 	t, err := template.ParseFiles("templates/table.html")
 	if err == nil {
-		t.ExecuteTemplate(w, "table", users)
+		t.ExecuteTemplate(w, "table", models.GetUsers())
 	} else {
 		w.Write([]byte(err.Error()))
 	}
--- week4/models/user.go
+++ week4/models/user.go
@@ -5,3 +5,11 @@ type User struct {
 	Email     string
 	Username  string
 }
+
+func GetUsers() []User {
+	users := make([]User, 3)
+	users[0] = User{"Some name", "test@test.com", "fdscxv"}
+	users[1] = User{"Another name", "hz@hz.hz", "aasd"}
+	users[2] = User{"Afdsi", "fff@asczx.net", "7asfgvsd"}
+	return users
+}
